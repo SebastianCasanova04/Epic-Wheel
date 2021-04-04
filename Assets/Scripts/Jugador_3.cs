a46bf9cfa4da16d4b8523450f6c991156da770ca
JaimeEsteban <81842732+JaimeEsteban@users.noreply.github.com>
2021-04-04 12:48:47 -0500

    inicio_nivel_3

---
+++ Assets/Scripts/Jugador_3.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador_3 : MonoBehaviour
+
+{
+    public float velocidad;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float movHorizontal = Input.GetAxis("Horizontal");
+    
+
+        Vector3 movimiento = new Vector3(movHorizontal,0.0f,0.0f);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            movimiento = new Vector3(movHorizontal,20,0.0f);
+        }
+        
+        rb.AddForce(movimiento*velocidad);
+    }
+}
